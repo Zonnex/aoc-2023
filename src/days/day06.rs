9608861a50d8f3a547e8214c75b4756d69c01b46
Conny Sjögren <conny.sjogren@gmail.com>
2023-12-06 07:56:58 +0100

    day 6 solved

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,20 +1,73 @@
 use crate::{Solution, SolutionPair};
 
-pub fn solve(_input: &str) -> SolutionPair {
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+pub fn solve(input: &str) -> SolutionPair {
+    (p1(input), p2(input))
+}
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+fn p1(input: &str) -> Solution {
+    parse_races(input)
+        .into_iter()
+        .map(|(time, record)| calculate_margin_of_error(time, record))
+        .product::<usize>()
+        .into()
+}
+
+fn p2(input: &str) -> Solution {
+    let values = input
+        .replace(" ", "")
+        .lines()
+        .filter_map(|line| {
+            let (_, value) = line.split_once(":").unwrap();
+            value.parse::<usize>().ok()
+        })
+        .collect::<Vec<_>>();
+
+    let time = values[0];
+    let record = values[1];
+
+    calculate_margin_of_error(time, record).into()
+}
+
+fn parse_races(input: &str) -> Vec<(usize, usize)> {
+    fn parse_line(line: &str) -> Vec<usize> {
+        let (_, values) = line.split_once(":").unwrap();
+        values
+            .split_whitespace()
+            .into_iter()
+            .map(str::parse::<usize>)
+            .filter_map(Result::ok)
+            .collect()
+    }
+    let times = parse_line(input.lines().next().unwrap());
+    let records = parse_line(input.lines().last().unwrap());
+
+    times.into_iter().zip(records.into_iter()).collect()
+}
+
+fn calculate_margin_of_error(time: usize, record: usize) -> usize {
+    (1..time)
+        .into_iter()
+        .filter_map(|charge| {
+            let remaining = time - charge;
+            let distance = charge * remaining;
+            if distance > record {
+                Some(charge)
+            } else {
+                None
+            }
+        })
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::etc::Solution;
 
     #[test]
     fn test_sample_input() {
         let input = include_str!("../../input/day06/test.txt");
-        let (_p1, _p2) = super::solve(input);
-        // assert_eq!(p1, expected);
-        // assert_eq!(p2, expected);
+        let (p1, p2) = super::solve(input);
+        assert_eq!(p1, Solution::USize(288));
+        assert_eq!(p2, Solution::USize(71503));
     }
 }
