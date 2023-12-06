90c0d4d9c6a2fdd91ef41b6d89dc8342c768b81f
Conny Sjögren <conny.sjogren@gmail.com>
2023-12-06 23:15:23 +0100

    day 6 optimized

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -5,58 +5,58 @@ pub fn solve(input: &str) -> SolutionPair {
 }
 
 fn p1(input: &str) -> Solution {
-    parse_races(input)
+    fn parse_line(line: &str) -> Vec<usize> {
+        line.split_whitespace()
+            .skip(1)
+            .map(str::parse::<usize>)
+            .filter_map(Result::ok)
+            .collect()
+    }
+    let (l1, l2) = input.split_once('\n').unwrap();
+    let times = parse_line(l1);
+    let records = parse_line(l2);
+
+    times
         .into_iter()
-        .map(|(time, record)| calculate_margin_of_error(time, record))
+        .zip(records.into_iter())
+        .map(|(t, d)| f(t, d))
         .product::<usize>()
         .into()
 }
 
 fn p2(input: &str) -> Solution {
-    let values = input
-        .replace(" ", "")
-        .lines()
-        .filter_map(|line| {
-            let (_, value) = line.split_once(":").unwrap();
-            value.parse::<usize>().ok()
-        })
-        .collect::<Vec<_>>();
-
-    let time = values[0];
-    let record = values[1];
-
-    calculate_margin_of_error(time, record).into()
-}
-
-fn parse_races(input: &str) -> Vec<(usize, usize)> {
-    fn parse_line(line: &str) -> Vec<usize> {
-        let (_, values) = line.split_once(":").unwrap();
-        values
-            .split_whitespace()
-            .into_iter()
-            .map(str::parse::<usize>)
-            .filter_map(Result::ok)
-            .collect()
+    fn parse_line(line: &str) -> usize {
+        line.split_whitespace()
+            .skip(1)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap()
     }
-    let times = parse_line(input.lines().next().unwrap());
-    let records = parse_line(input.lines().last().unwrap());
+    let (l1, l2) = input.split_once('\n').unwrap();
+    let times = parse_line(l1);
+    let records = parse_line(l2);
 
-    times.into_iter().zip(records.into_iter()).collect()
+    f(times, records).into()
 }
 
-fn calculate_margin_of_error(time: usize, record: usize) -> usize {
-    (1..time)
-        .into_iter()
-        .filter_map(|charge| {
-            let remaining = time - charge;
-            let distance = charge * remaining;
-            if distance > record {
-                Some(charge)
-            } else {
-                None
-            }
-        })
-        .count()
+fn f(t: usize, d: usize) -> usize {
+    let (t, d) = (t as f64, d as f64);
+    let center = t / 2.0;
+    let diff = ((t / 2.0).powi(2) - d).sqrt();
+
+    let root1 = match center - diff {
+        x @ _ if x.fract() == 0.0 => x + 1.0,
+        x => x.ceil(),
+    } as usize;
+
+    let root2 = match center + diff {
+        x @ _ if x.fract() == 0.0 => x - 1.0,
+        x => x.floor(),
+    } as usize;
+    
+    let c1 = root1 as usize;
+    let c2 = root2 as usize;
+    (c1..=c2).count()
 }
 
 #[cfg(test)]
