a518dd2a4c31af86c0a98943490bb4e252f6b408
Conny Sjögren <conny.sjogren@gmail.com>
2023-12-07 19:13:37 +0100

    day 7 part 1

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,20 +1,72 @@
 use crate::{Solution, SolutionPair};
 
-pub fn solve(_input: &str) -> SolutionPair {
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+pub mod camel_cards;
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+#[derive(Debug, Eq)]
+struct Line(camel_cards::Hand, camel_cards::Bid);
+
+impl Ord for Line {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
+impl PartialOrd for Line {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl PartialEq for Line {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+pub fn solve(input: &str) -> SolutionPair {
+    let mut lines = input
+        .lines()
+        .map(|line| {
+            let (hand, bid) = line.split_once(" ").unwrap();
+            let hand = camel_cards::Hand::parse(hand);
+            let bid = camel_cards::Bid(bid.parse().unwrap());
+            Line(hand, bid)
+        })
+        .collect::<Vec<_>>();
+
+    lines.sort();
+
+    (p1(&lines), p2(input))
+}
+
+fn p1(input: &[Line]) -> Solution {
+    let winnings = input
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, Line(_, camel_cards::Bid(bid)))| {
+            let rank = i + 1;
+            let bid = *bid as usize;
+            rank * bid
+        })
+        .sum::<usize>();
+
+    Solution::USize(winnings)
+}
+
+fn p2(input: &str) -> Solution {
+    Solution::U64(0)
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::etc::Solution;
 
     #[test]
     fn test_sample_input() {
         let input = include_str!("../../input/day07/test.txt");
-        let (_p1, _p2) = super::solve(input);
-        // assert_eq!(p1, expected);
+        let (p1, _p2) = super::solve(input);
+        assert_eq!(p1, Solution::USize(6440));
         // assert_eq!(p2, expected);
     }
 }
---
+++ src/days/day07/camel_cards.rs
@@ -0,0 +1,233 @@
+use std::collections::HashMap;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Hash)]
+pub struct Bid(pub u32);
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
+pub enum Card {
+    A,
+    K,
+    Q,
+    J,
+    T,
+    N(u8),
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.value().partial_cmp(&other.value())
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.value().cmp(&other.value())
+    }
+}
+
+impl Card {
+    pub fn from_char(s: char) -> Option<Self> {
+        match s {
+            'A' => Some(Card::A),
+            'K' => Some(Card::K),
+            'Q' => Some(Card::Q),
+            'J' => Some(Card::J),
+            'T' => Some(Card::T),
+            '2'..='9' => s.to_digit(10).map(|n| Card::N(n as u8)),
+            _ => None,
+        }
+    }
+
+    pub fn value(&self) -> u8 {
+        match self {
+            Card::A => 14,
+            Card::K => 13,
+            Card::Q => 12,
+            Card::J => 11,
+            Card::T => 10,
+            Card::N(n) => *n,
+        }
+    }
+
+    pub fn parse(s: &str) -> Vec<Self> {
+        s.chars().filter_map(|c| Card::from_char(c)).collect()
+    }
+}
+
+#[derive(Debug, Clone, Eq)]
+pub struct Hand {
+    pub cards: Vec<Card>,
+    card_count: HashMap<Card, u8>,
+}
+
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.cards == other.cards
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.rank() == other.rank() {
+            self.cards.cmp(&other.cards)
+        } else {
+            self.rank().cmp(&other.rank())
+        }
+    }
+}
+
+impl Hand {
+    pub fn new(cards: Vec<Card>) -> Self {
+        let mut count = HashMap::new();
+        for card in &cards {
+            *count.entry(*card).or_insert(0) += 1;
+        }
+
+        Self {
+            cards,
+            card_count: count,
+        }
+    }
+
+    pub fn rank(&self) -> u8 {
+        if self.five_of_a_kind() {
+            return 6;
+        }
+        if self.four_of_a_kind() {
+            return 5;
+        }
+        if self.full_house() {
+            return 4;
+        }
+        if self.three_of_a_kind() {
+            return 3;
+        }
+        if self.two_pair() {
+            return 2;
+        }
+        if self.one_pair() {
+            return 1;
+        }
+        0
+    }
+
+    pub fn five_of_a_kind(&self) -> bool {
+        let count = &self.card_count;
+        count.values().any(|&v| v == 5)
+    }
+
+    pub fn four_of_a_kind(&self) -> bool {
+        let count = &self.card_count;
+        count.values().any(|&v| v == 4)
+    }
+
+    pub fn full_house(&self) -> bool {
+        let count = &self.card_count;
+        count.values().any(|&v| v == 3) && count.values().any(|&v| v == 2)
+    }
+
+    pub fn three_of_a_kind(&self) -> bool {
+        let count = &self.card_count;
+        count.values().any(|&v| v == 3) && count.values().filter(|&v| *v == 1).count() == 2
+    }
+
+    pub fn two_pair(&self) -> bool {
+        let count = &self.card_count;
+        count.values().filter(|&v| *v == 2).count() == 2
+    }
+
+    pub fn one_pair(&self) -> bool {
+        let count = &self.card_count;
+        count.values().any(|&v| v == 2) && count.values().filter(|&v| *v == 1).count() == 3
+    }
+
+    pub(crate) fn parse(hand: &str) -> Hand {
+        let cards = Card::parse(hand);
+        Hand::new(cards)
+    }
+}
+
+#[cfg(test)]
+mod hand_tests {
+    use super::*;
+
+    #[test]
+    fn test_five_of_a_kind() {
+        let hand = Hand::new(vec![Card::A, Card::A, Card::A, Card::A, Card::A]);
+        assert!(hand.five_of_a_kind());
+    }
+
+    #[test]
+    fn test_four_of_a_kind() {
+        let hand = Hand::new(vec![Card::A, Card::A, Card::A, Card::A, Card::K]);
+        assert!(hand.four_of_a_kind());
+    }
+
+    #[test]
+    fn test_full_house() {
+        let hand = Hand::new(vec![Card::A, Card::A, Card::A, Card::N(2), Card::N(2)]);
+        assert!(hand.full_house());
+    }
+
+    #[test]
+    fn test_three_of_a_kind() {
+        let hand = Hand::new(vec![Card::A, Card::A, Card::A, Card::N(2), Card::N(3)]);
+        assert!(hand.three_of_a_kind());
+    }
+
+    #[test]
+    fn test_two_pair() {
+        let hand = Hand::new(vec![Card::A, Card::A, Card::N(2), Card::N(2), Card::N(3)]);
+        assert!(hand.two_pair());
+    }
+
+    #[test]
+    fn test_one_pair() {
+        let hand = Hand::new(vec![Card::A, Card::A, Card::N(2), Card::N(3), Card::N(4)]);
+        assert!(hand.one_pair());
+    }
+
+    #[test]
+    fn test_sorting() {
+        let a = Hand::parse("AAAA6");
+        let b = Hand::parse("AQAAA");
+
+        assert!(a > b);
+    }
+}
+
+#[cfg(test)]
+mod card_tests {
+    use super::*;
+
+    #[test]
+    fn test_card_sorting() {
+        let mut cards = Card::parse("987A645KQJT32");
+
+        cards.sort();
+        assert_eq!(
+            cards,
+            vec![
+                Card::A,
+                Card::K,
+                Card::Q,
+                Card::J,
+                Card::T,
+                Card::N(9),
+                Card::N(8),
+                Card::N(7),
+                Card::N(6),
+                Card::N(5),
+                Card::N(4),
+                Card::N(3),
+                Card::N(2),
+            ]
+        );
+    }
+}
