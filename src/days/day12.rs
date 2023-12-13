15a1ccc688fa54c5746f49e633ca0b8d41121e58
Conny Sjögren <conny.sjogren@gmail.com>
2023-12-13 07:38:16 +0100

    day12 wip

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,20 +1,113 @@
+use std::ops::RangeInclusive;
+
 use crate::{Solution, SolutionPair};
 
-pub fn solve(_input: &str) -> SolutionPair {
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+struct Input {
+    lines: Vec<Line>,
+}
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+impl Input {
+    fn parse(input: &str) -> Self {
+        let lines = input
+            .lines()
+            .map(|line| {
+                let (springs, pattern) = line.split_once(" ").unwrap();
+                let springs = springs.to_owned();
+                let pattern = pattern
+                    .split(',')
+                    .filter_map(|s| usize::from_str_radix(s, 10).ok())
+                    .collect::<Vec<_>>();
+                Line { springs, pattern }
+            })
+            .collect();
+        Self { lines }
+    }
+}
+
+struct Line {
+    springs: String,
+    pattern: Vec<usize>,
+}
+
+pub fn solve(input: &str) -> SolutionPair {
+    (p1(input), p2(input))
+}
+
+fn p1(input: &str) -> Solution {
+    let input = Input::parse(input);
+    let total_arrangements = input
+        .lines
+        .iter()
+        .map(|l| count_arrangements(&l.springs, &l.pattern))
+        .sum::<usize>();
+
+    Solution::Usize(total_arrangements)
+}
+
+fn count_arrangements(pattern: &str, solution: &[usize]) -> usize {
+    let current = 0;
+    let ranges = initiate_ranges(pattern, solution);
+    for size in solution {}
+    // if solution length is 1, this is the last window, count all valid positions to the right
+    // if solution length is greater than 1, recurse with smaller solution and substring of remaining pattern
+    // after recurse, move to the right until valid, then recurse again
+    // make inclusive range of size equal to first usize in solution
+    // find first spot in pattern for that range so that all elements matches ? or #
+    // surrounding elements must be ., ?, or out of bounds
+
+    /*
+    ??##.?????#?? 3,1,2
+    [??#]#[.]?[??]??#??
+    ?[?##].[?]????#??
+    ?[?##].[?]?[??]?#??
+
+    ?[?##].[?]???[?#]?? = 1
+    ?[?##].[?]????[#?]? = 1
+    ?[?##].?[?]??[?#]?? = 1
+    ?[?##].?[?]???[#?]? = 1
+    ?[?##].??[?]?[?#]?? = 1
+    ?[?##].??[?]??[#?]? = 1
+    ?[?##].???[?]?[#?]? = 1
+
+    // all groups satisfied, no # outside groups
+    */
+    0
+}
+
+fn initiate_ranges(pattern: &str, solution: &[usize]) -> Vec<RangeInclusive<usize>> {
+    let mut ranges = Vec::new();
+    let mut start = 0;
+    for size in solution {
+        let end = start + size - 1;
+        let mut valid_range = false;
+
+        while !valid_range {
+            valid_range = pattern[start..=end].chars().all(|c| c == '?' || c == '#')
+                && pattern.chars().nth(start - 1) != Some('#')
+                && pattern.chars().nth(end + 1) != Some('#');
+                
+            start += 1;
+        }
+
+        ranges.push(start..=end);
+        start = end + 1;
+    }
+    ranges
+}
+
+fn p2(input: &str) -> Solution {
+    Solution::Usize(0)
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::etc::Solution;
 
     #[test]
     fn test_sample_input() {
         let input = include_str!("../../input/day12/test.txt");
-        let (_p1, _p2) = super::solve(input);
-        // assert_eq!(p1, expected);
-        // assert_eq!(p2, expected);
+        let (p1, p2) = super::solve(input);
+        assert_eq!(p1, Solution::Usize(21));
+        assert_eq!(p2, Solution::Usize(0));
     }
 }
